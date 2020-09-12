3c45a83629dd27bf88ab27275a05f11e12d24e66
americann <estserrow.k2@gmail.com>
2020-09-12 17:34:54 +0900

    プロジェクト ファイルを追加します。

---
+++ TenKeyPad/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(function () {
+    $("#defaultKeypad").keypad({
+        keypadOnly: false,
+    });
+    $('#defaultKeypadd').keypad();
+
+});
+
+//DevTools failed to load SourceMap: Could not load content for https://localhost:44393/js/jquery.plugin.min.map:
+// HTTP error: status code 404, net:: ERR_HTTP_RESPONSE_CODE_FAILURE
